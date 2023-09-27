d2b3c93775f2ee10cfafbcafba605e4685b8d988
mansao01 <novrilukman15@gmail.com>
2023-09-27 18:18:35 +0700

    create several endpoint for note

--- routes/NoteRoute.js
+++ routes/NoteRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {verifyToken} from "../middleware/VerifyToken.js";
-import {getNotesById} from "../controller/Note.js";
+import {getNotesById,getNotes, createNote, updateNote, deleteNote} from "../controller/Note.js";
 
 const router = express.Router();
 
 
-router.get("/v1/note/:userId", verifyToken, getNotesById)
+router.get("/v1/notes/:userId", verifyToken, getNotesById)
+router.get("/v1/notes", getNotes)
+router.post("/v1/note", verifyToken, createNote)
+router.delete("/v1/note", verifyToken, deleteNote)
+router.patch("/v1/note", verifyToken, updateNote)// not tested yet
 
 export default router;
